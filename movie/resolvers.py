b38d658e8a286763e07fd6ae234f395dd3a62697
LouisMDK <louis.painter49@gmail.com>
2023-11-14 15:02:54 +0100

    graphql movies

--- movie/resolvers.py
+++ movie/resolvers.py
@@ -1,6 +1,30 @@
 import json
 
 
+def get_all_movies(_, info):
+    with open('{}/data/movies.json'.format("."), "r") as file:
+        newmovies = json.load(file)
+        return newmovies['movies']
+
+
+def create_movie(_, info, _id, _title, _director, _rating):
+    newmovies = {}
+    new = None
+    with open('{}/data/movies.json'.format("."), "r") as file:
+        newmovies = json.load(file)
+        for movie in newmovies['movies']:
+            if movie['id'] == _id:
+                return new
+        new = {'id': _id, 'title': _title, 'director': _director, 'rating': _rating}
+        newmovies['movies'].append(
+            new
+        )
+    if new is not None:
+        with open('{}/data/movies.json'.format("."), "w") as wfile:
+            json.dump(newmovies, wfile)
+    return new
+
+
 def movie_with_id(_, info, _id):
     with open('{}/data/movies.json'.format("."), "r") as file:
         movies = json.load(file)
@@ -16,6 +40,7 @@ def movie_with_title(_, info, _title):
             if movie['title'] == _title:
                 return movie
 
+
 def movie_with_director(_, info, _director):
     with open('{}/data/movies.json'.format("."), "r") as file:
         movies = json.load(file)
@@ -26,17 +51,18 @@ def movie_with_director(_, info, _director):
 
 def delete_movie(_, info, _id):
     newmovies = {}
-    deleted = {}
+    deleted = None
     with (open('{}/data/movies.json'.format("."), "r") as rfile):
         movies = json.load(rfile)
         for index, movie in enumerate(movies['movies']):
             if movie['id'] == _id:
                 deleted = movie
-                del movies[index]
+                del movies['movies'][index]
                 newmovies = movies
                 break
-    with open('{}/data/movies.json'.format("."), "w") as wfile:
-        json.dump(newmovies, wfile)
+    if deleted is not None:
+        with open('{}/data/movies.json'.format("."), "w") as wfile:
+            json.dump(newmovies, wfile)
     return deleted
 
 
