dc49ddbb828d17b81acc9df646b4f6b67f421f2d
LouisMDK <louis.painter49@gmail.com>
2023-11-21 15:57:42 +0100

    fix add booking user

--- booking/booking.py
+++ booking/booking.py
@@ -55,21 +55,17 @@ class BookingServicer(booking_pb2_grpc.BookingServicer):
         movieid = request.movieid
         moviedate = request.date
 
-        # verify the booking is available in the showtime servicer
         try:
             with grpc.insecure_channel(f"{showtimeHost}:{showtimePort}") as channel:
                 showTimeStub = showtime_pb2_grpc.ShowTimesStub(channel)
                 showTimeDate = showtime_pb2.Date(date=moviedate)
                 showtime = showTimeStub.GetScheduleByDate(showTimeDate)
         except Exception as e:
-            # if booking can't be done, returning an error object, with userId to -1
-            return booking_pb2.Book(userid="-1", dates=[])
+            return booking_pb2.Book(userid="", dates=[])
 
         if showtime.date == -1 or movieid not in showtime.movies:
-            # if booking can't be done, returning an error object, with userId to -1
-            return booking_pb2.Book(userid="-1", dates=[])
+            return booking_pb2.Book(userid="", dates=[])
 
-        # get user or creating it if don't already exist
         userIndex = None
         for i, userBooking in enumerate(self.db):
             if str(userBooking["userid"]) == str(request.userid):
@@ -81,11 +77,10 @@ class BookingServicer(booking_pb2_grpc.BookingServicer):
             self.db.append(user)
             userIndex = len(self.db) - 1
 
-        # get date or creating it if don't already exist
-        date = None
-        for userDate in user['dates']:
-            if userDate["date"] == request.date:
-                date = userDate
+        dateIndex = None
+        for i, userDate in enumerate(self.db[userIndex]['dates']):
+            if str(userDate["date"]) == str(request.date):
+                dateIndex = i
                 break
 
         if dateIndex is None:
@@ -93,14 +88,10 @@ class BookingServicer(booking_pb2_grpc.BookingServicer):
             self.db[userIndex]['dates'].append(date)
             dateIndex = len(self.db[userIndex]['dates']) - 1
 
-        date['movies'].append(request.movieid)
-        user['dates'].append(date)
-
-        # save change in data
-        with open('{}/data/bookings.json'.format("."), "r") as jsf:
+        self.db[userIndex]['dates'][dateIndex]['movies'].append(request.movieid)
+        with open('{}/data/bookings.json'.format("."), "w") as jsf:
             json.dump(self.db, jsf)
 
-        # like in REST convention, returning the just created object
         return booking_pb2.Book(userid=request.userid,
                                 dates=[booking_pb2.BookingDate(
                                     date=self.db[userIndex]['dates'][index]['date'],
